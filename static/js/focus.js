8cb6076eddb19b5111346c38f750f09c78a28a74
crane719 <tmp.utils@gmail.com>
2021-07-25 15:22:55 +0900

    文字が選択されないようなクリックの場合のみinputをfocusするように修正

--- static/js/focus.js
+++ static/js/focus.js
@@ -3,7 +3,23 @@
   tabはautocompleteで別処理
 */
 
-function forciblyFocus() {
+function focusLatestInput() {
   var latestInput = document.getElementsByClassName("latest-input")[0];
   latestInput.focus();
 }
+
+// 文字が選択されていないケースのclickの場合のみfocusするように
+// https://qiita.com/uuuno/items/5a215d9bfeabec8adbc3 を参考に
+var terminal = document.getElementsByClassName("terminal")[0];
+function upListener(e){
+  if (window.getSelection){
+    var selectedStr = window.getSelection().toString();  //文章取得
+    if(!(selectedStr !== '' && selectedStr !== '\n')){  //文章チェック
+      focusLatestInput();
+    }
+  }else{
+    /* getSelectionがないケースがわからないが念の為 */
+    focusLatestInput();
+  }
+}
+terminal.addEventListener("mouseup", upListener);
