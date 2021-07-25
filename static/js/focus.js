e5428b38cb8b70b192f01965410c3f4e8df52ab2
crane719 <tmp.utils@gmail.com>
2021-07-25 14:30:50 +0900

    focus.jsにコメント追加

--- static/js/focus.js
+++ static/js/focus.js
@@ -1,3 +1,8 @@
+/*
+  inputからfocusが消えてしまうようなケースを対処
+  tabはautocompleteで別処理
+*/
+
 function forciblyFocus() {
   var latestInput = document.getElementsByClassName("latest-input")[0];
   latestInput.focus();
