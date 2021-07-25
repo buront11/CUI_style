d2893e465d33475ca3c2cdeeb48eb586b50d9325
crane719 <tmp.utils@gmail.com>
2021-07-25 16:36:59 +0900

    formatter

--- static/js/focus.js
+++ static/js/focus.js
@@ -11,13 +11,14 @@ function focusLatestInput() {
 // 文字が選択されていないケースのclickの場合のみfocusするように
 // https://qiita.com/uuuno/items/5a215d9bfeabec8adbc3 を参考に
 var terminal = document.getElementsByClassName("terminal")[0];
-function upListener(e){
-  if (window.getSelection){
-    var selectedStr = window.getSelection().toString();  //文章取得
-    if(!(selectedStr !== '' && selectedStr !== '\n')){  //文章チェック
+function upListener(e) {
+  if (window.getSelection) {
+    var selectedStr = window.getSelection().toString(); //文章取得
+    if (!(selectedStr !== "" && selectedStr !== "\n")) {
+      //文章チェック
       focusLatestInput();
     }
-  }else{
+  } else {
     /* getSelectionがないケースがわからないが念の為 */
     focusLatestInput();
   }
