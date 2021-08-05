6d571aa6fdfdc3aa6980967bb2d4ca2020bde54b
kkuriyama <s193129@stn.nagaokaut.ac.jp>
2021-08-05 17:25:52 +0900

    入力欄が下に遷移するように変更． Co-authored-by: buront11 <buront11@users.noreply.github.com>

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+
 function display(element) {
   ReactDOM.render(element, document.getElementsByClassName("terminal")[0]);
 }
@@ -14,30 +15,52 @@ function InitialMessageComponent() {
 }
 
 function InputComponent(props) {
+  const [text, setText] = React.useState('')
+  
+  const HandleChange = (e) => {
+    setText(() => e.target.value)
+  }
+
   return (
     <div class="line">
       <span class="user">{props.name}:</span>
       <span class="location">{props.directory}</span>
       <span class="percent">%</span>
-      <input type="text" class="latest-input" onKeyPress={OnEnterPressed} autoFocus={true} />
+      <input type="text" value={text} class="latest-input" onChange={HandleChange} onKeyPress={props.enterFunction} autoFocus={true} 
+      disabled={props.disableInput} />
     </div>
   );
 }
 
-function OnEnterPressed(e) {
-  if (e.key == 'Enter'){
-    alert('Pressed Enter')
-  }
-}
-
 function Terminal(props) {
-  return (
-    <div>
-      <InitialMessageComponent />
+  const [count, setCount] = React.useState([false])
+
+  const OnEnterPressed = (e) => {
+    if (e.key == 'Enter'){
+      let newCount = []
+      count.map(() => {
+        newCount.push(true)
+      })
+      setCount([...newCount, false])
+      console.log(count)
+    }
+  }
+
+  const CommandLines = count.map((value, idx) => {
+    return (
       <InputComponent
         name={props.userName}
         directory={props.currentDirectory}
+        enterFunction={OnEnterPressed}
+        disableInput={value}
       />
+    );
+  }); 
+  
+  return (
+    <div>
+      <InitialMessageComponent />
+      {CommandLines}
     </div>
   );
 }
